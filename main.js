b3176d8dd6fc163103ef0d2b5ccd6f7d4ea8953c
dathegreat <d.a.thegreat123@gmail.com>
2022-11-11 16:48:28 -0500

    Finally got arrows pointing in the right direction at the vector tips, and made some headway into transformation matrix user entry

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const transform = (a, b) =>{
     return new Vector(product[0], product[1])
 }
 
-const multiply = (a, b) =>{
+const multiplyVectors = (a, b) =>{
     //takes a vector and a scalar
     //returns a 2x1 vector
     return ( new Vector(
@@ -21,12 +21,29 @@ const multiply = (a, b) =>{
     ))
 }
 
+const addVectors = (a, b) =>{
+    return(
+        new Vector(a.x + b.x, a.y + b.y)
+    )
+}
+
 class Vector{
     constructor(x, y){
         this.x = x
         this.y = y
         this.magnitude = Math.sqrt(
             Math.pow(this.x, 2) + Math.pow(this.y, 2)
+        )
+    }
+    getNormal(){
+        return(
+            transform(
+                new Matrix([
+                    new Vector(0, -1),
+                    new Vector(1, 0)
+                ]), 
+            new Vector(this.x, this.y)
+            )
         )
     }
 }
@@ -46,7 +63,6 @@ class Matrix{
             const _y = row.y
             tempMatrix.push( new Vector(_y, _x) )
         })
-        console.log(new Matrix(tempMatrix))
         return new Matrix(tempMatrix)
     }
 }
@@ -55,28 +71,34 @@ const drawVector = (vector, start= new Vector(0,0) ) =>{
     const canvas = document.getElementById("canvas")
     const ctx = canvas.getContext("2d")
     const thickness = 10
-    const tip = new Vector(start.x + vector.x, start.y + vector.y)
-    const normal = transform(
-        new Matrix([
-            new Vector(0, -1),
-            new Vector(1, 0)
-        ]
-    ), vector)
-    const unitNormal = multiply(normal, 1/vector.magnitude)
-        
+    const tip = addVectors(vector, start)
+    const unitNormal = multiplyVectors(vector.getNormal(), 1/vector.magnitude)
+    const scaledNormal = multiplyVectors(unitNormal, thickness)
+    const scaledVector = multiplyVectors(vector, 0.9)
+    const scaledTip = addVectors(scaledVector, start)
+    const arrowTop = addVectors(
+        scaledTip, 
+        scaledNormal
+    )
+    const arrowBottom = addVectors(
+        scaledTip, 
+        multiplyVectors(scaledNormal, -1)
+    )
+    //draw vector
     ctx.lineWidth = thickness
     ctx.strokeStyle = "black"
-
     ctx.beginPath()
     ctx.moveTo(start.x, start.y)
-    ctx.lineTo(tip.x, tip.y)
-
-    ctx.moveTo(tip.x - thickness, tip.y)
-    ctx.lineTo(tip.x - thickness, tip.y + thickness)
-    ctx.lineTo(tip.x, tip.y)
-    ctx.lineTo(tip.x - thickness, tip.y - thickness)
-    ctx.lineTo(tip.x - thickness, tip.y)
+    ctx.lineTo(scaledTip.x, scaledTip.y)
     ctx.stroke()
+    //draw arrow tip
+    ctx.beginPath()
+    ctx.fillStyle="black"
+    ctx.moveTo(tip.x, tip.y)
+    ctx.lineTo(arrowTop.x, arrowTop.y)
+    ctx.lineTo(arrowBottom.x, arrowBottom.y)
+    ctx.closePath()
+    ctx.fill()
 }
 
 const drawMatrix = (matrix) =>{
@@ -95,13 +117,22 @@ const drawGrid = (identity) =>{
     }
 }
 
+const handleMatrixInput = () =>{
+    const transformationMatrix = new Matrix(
+        document.forms['matrixInput']['[0,0]'].value,
+        document.forms['matrixInput']['[1,0]'].value,
+        document.forms['matrixInput']['[0,1]'].value,
+        document.forms['matrixInput']['[1,1]'].value,
+    )
+    console.log(transformationMatrix)
+}
+
 document.getElementById("canvas").height = canvasSize
 document.getElementById("canvas").size = canvasSize
+document.getElementById("matrixInput").addEventListener("onchange", handleMatrixInput)
 const grid = new Matrix([
     new Vector(scale, 0),
     new Vector(0, scale)
     ])
 
-//drawGrid(grid)
-drawVector(new Vector(100,100))
-console.log(grid.rows)
+drawGrid(grid)
